d376689f50e274f44f884ce1ba501ab4af28c1d1
Amritanshu Keshari <84897578+AnshuWorld@users.noreply.github.com>
2024-08-29 17:14:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function fun() {
+    var nav = document.getElementById('navbar');
+    var res = 'resp';
+    nav.classList.toggle(res);
+}
+function myFun() {
+    var ont = document.body;
+    ont.classList.toggle("dark");
+}
