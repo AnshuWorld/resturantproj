61c80cc5a97e47307650a9fe66e4b6331799babd
Amritanshu Keshari <84897578+AnshuWorld@users.noreply.github.com>
2024-08-30 10:35:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function fun() {
-    var nav = document.getElementById('navbar');
-    var res = 'resp';
-    nav.classList.toggle(res);
-}
-function myFun() {
-    var ont = document.body;
-    ont.classList.toggle("dark");
-}
